29821dd0128416b8dbd98cbc6d369f018708ead8
Randolph Pe Benito <randolph@xy-randolph.xyber.ph>
2017-01-16 11:38:00 +0800

    Added urls.

--- miniature/miniature/urls.py
+++ miniature/miniature/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from url_short import views
-#from dashing.utils import router
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^dashboard/', include('dashing.urls')),
     url(r'^shorten/(?P<pk>[0-9]+)/$', views.show_url_shorten, name='url_shorten'),
     url(r'^(?P<urlshort>[a-zA-Z0-9]{7})/$', views.redirect_url_shorten, name='url_shorten_redirect'),
 
